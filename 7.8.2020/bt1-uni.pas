2093990eb36f73d319d8f62fd9eb4711e94d006a
Nguyễn Phạm Gia Bảo <giabaodoremon@gmail.com>
2020-08-11 11:39:40 +0700

    Add Vietnamese notice in readme.md and new excercises from 7th August

---
+++ 7.8.2020/bt1-uni.pas
@@ -0,0 +1,46 @@
+//Nhập 1 số  tính tổng lập phương các số nguyên tố của số đó
+program bt1;
+
+const filein='bt1in.txt'; fileout='bt1out.txt';
+
+var n:int64; f:text;
+
+procedure readfile;
+ begin
+  writeln('Đang đọc từ  file bt1in.txt...');
+  assign(f,filein);
+  reset(f);
+  readln(f,n);
+  close(f);
+ end;
+
+function chkprime(x:int64):boolean;
+ var i:int64;
+ begin
+  if x<2 then exit(false);
+  for i:=2 to trunc(sqrt(x)) do
+  if x mod i = 0 then exit(false);
+  exit(true);
+ end;
+
+procedure processnout;
+ var rem,t:byte;
+ begin
+  writeln('Đang xử  lý và viết kết quả ra file bt1out.txt...');
+  assign(f,fileout);
+  rewrite(f);
+  while n>0 do
+  begin
+   rem:=n mod 10;
+   if chkprime(rem) then t:=t+(rem*rem*rem);
+   n:=n div 10;
+  end;
+  if t>0 then writeln(f,t) else writeln(f,0);
+  close(f);
+ end;
+
+begin
+ readfile;
+ processnout;
+ writeln('Đã xong.');
+end.          
---
+++ hello-world-uni.pas
@@ -0,0 +1,7 @@
+program helloworld;
+uses crt;
+
+begin
+ writeln('Xin chào đây là hello world bằng tiếng việt CÓ DẤu :))');
+ readln();
+end. 
